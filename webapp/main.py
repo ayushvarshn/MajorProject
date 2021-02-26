c4ca0a76a1a27d26c06d0657078c83c1362b844e
freddy10195 <amansingh160899@gmail.com>
2021-02-27 00:48:39 +0530

    update webapp with bootstrap

--- webapp/main.py
+++ webapp/main.py
@@ -3,7 +3,7 @@ import random
 import time
 from datetime import datetime
 
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, url_for
 
 application = Flask(__name__)
 random.seed()  # Initialize the random number generator
@@ -11,7 +11,23 @@ random.seed()  # Initialize the random number generator
 
 @application.route('/')
 def index():
-    return render_template('mainpage.html')
+    return render_template('main.html')
+
+@application.route('/temperature')
+def temp():
+    return render_template('temp.html')
+
+@application.route('/voltage')
+def voltage():
+    return render_template('voltage.html')
+
+@application.route('/soc')
+def soc():
+    return render_template('soc.html')
+
+@application.route('/soh')
+def soh():
+    return render_template('soh.html')
 
 
 @application.route('/chart-data')
@@ -19,7 +35,7 @@ def chart_data():
     def generate_random_data():
         while True:
             json_data = json.dumps(
-                {'time': datetime.now().strftime('%M:%S'), 'temp': random.randint(25, 35), 'voltage': random.randint(25, 35), 'soc': random.randint(90, 100), 'soh': random.randint(90, 100)})
+                {'time': datetime.now().strftime('%M:%S'), 'temp': random.randint(25, 35), 'voltage': random.randint(25, 35)/10, 'soc': random.randint(90, 100), 'soh': random.randint(90, 100)})
             yield f"data:{json_data}\n\n"
             time.sleep(1)
 
